73784c18dea5cbaef3a8111e6131dc9eb34367fe
zhulf0804 <zhulf0804@gmail.com>
2018-01-03 22:23:00 +0800

    20_class

--- 20_parentheses.cpp
+++ 20_parentheses.cpp
@@ -1,31 +1,29 @@
-#include "iostream"
-#include "string"
-#include "stack"
-using namespace std;
-
-int main(){
-	string s;
-	cin >> s;
-	stack<char> left;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> left;
 	
-	int len = s.length();
-	for(int i=0; i<len; i++){
-		if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-			left.push(s[i]);
-			continue;
-		}
-		if(!left.empty()){
-			if((s[i]==')'&&left.top()=='(')||(s[i]=='}'&&left.top()=='{')||(s[i]==']'&&left.top()=='[')){
-			continue;
-			}else{
-				cout << "false" << endl;
-				return 0;
-			}
-		}else{
-			cout << "false" <<endl;
-			return 0;
-		}
-	}
+        int len = s.length();
+        for(int i=0; i<len; i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                left.push(s[i]);
+                continue;
+            }
+            if(!left.empty()){
+                if((s[i]==')'&&left.top()=='(')||(s[i]=='}'&&left.top()=='{')||(s[i]==']'&&left.top()=='[')){
+                    left.pop();
+                    continue;
+                }else{
+                   
+                    return false;
+                }
+            }else{
+                
+                return false;
+            }
+        }
 
-	return 0;
-}
+        if(left.empty()) return true;
+        else return false;
+    } 
+};
