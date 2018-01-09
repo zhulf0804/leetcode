c90100198a0d688285a282039be11e1337d5ff7d
zhulf0804 <zhulf0804@gmail.com>
2018-01-09 19:54:17 +0800

    21_merged.cpp

---
+++ 21_merged.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for isngly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *ans = new ListNode(-1), *p = ans;
+        
+        while(l1&&l2){
+            if(l1->val<=l2->val){
+                p->next = l1;
+                l1 = l1 -> next;    
+            }else{
+                
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p= p->next;
+            
+        }
+        if(l1 == NULL){
+            p->next = l2;
+        }
+        if(l2 == NULL){
+            p->next = l1;
+        }
+        
+        
+        return ans->next;
+    }
+};
+
+
+/*//网上参考答案
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(-1), *cur = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        cur->next = l1 ? l1 : l2;
+        return dummy->next;
+    }
+};
+*/
